5bfef2a54cdbde3c7c7c0facba296fab9df42349
m0sk1t <m0sk1t@bk.ru>
2023-09-24 01:17:25 +0400

    Fix indents for filterSlice

--- src/components/redux/filterSlice.js
+++ src/components/redux/filterSlice.js
@@ -1,11 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const filterSlice = createSlice({
-    name: 'filter',
-    initialState: '',
-    reducers: {
-        setContactFilter: (state, action) => action.payload,
-    }
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setContactFilter: (state, action) => action.payload,
+  }
 });
 
 export const { setContactFilter } = filterSlice.actions;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
 import { Provider } from 'react-redux';
-import {store} from "../src/components/redux/store";
+import ReactDOM from 'react-dom/client';
+
+import { App } from 'components/App';
+
+import { store } from "../src/components/redux/store";
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
